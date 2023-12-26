b94c3c0053f7eacbe18a7dc9979fc47cf6ba6dc3
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 12:07:07 +0530

    Using useState hook

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import './Search.jsx';
-
-let searchTerm = '';
-
-function handleChange(event){
-  console.log(event.target.value);
-}
+import React, { useState } from "react";
+import "./Search.jsx";
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <span>
       <label htmlFor="search">Search : </label>
-      &ensp; <input id="search" type="text" onChange={handleChange}/>
+      &ensp; <input id="search" type="text" onChange={handleChange} />
       &ensp; | &ensp; Searching for <strong>{searchTerm}</strong>
     </span>
   );
