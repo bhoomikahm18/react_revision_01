03eaffd121459bc4409ac2261ff424cbfbefb788
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 12:43:58 +0530

    React fragment shorthand

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import "./Search.jsx";
 
 export default function Search(props) {
-
   function handleChange(event) {
     props.setSearchTerm(event.target.value);
   }
 
   return (
-    <span>
+    <>
       <label htmlFor="search">Search : </label>
-      &ensp; <input id="search" type="text" onChange={handleChange} value={props.searchTerm}/>
+      &ensp;{" "}
+      <input
+        id="search"
+        type="text"
+        onChange={handleChange}
+        value={props.searchTerm}
+      />
       &ensp; | &ensp; Searching for <strong>{props.searchTerm}</strong>
-    </span>
+    </>
   );
 }
