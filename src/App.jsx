86ca5ee1d994d10b25b3cbe1c65b4b98b985dd01
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 11:48:23 +0530

    Adding new elements to list

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,30 @@ const list = [
     points: 5,
     objectId: 1,
   },
+  {
+    title: "Angular",
+    url: "https://angular.io/",
+    author: "Google",
+    num_comments: 4,
+    points: 2,
+    objectId: 3,
+  },
+  {
+    title: "Vue JS",
+    url: "https://vuejs.org/",
+    author: "Evan You",
+    num_comments: 6,
+    points: 3,
+    objectId: 4,
+  },
+  {
+    title: "Next JS",
+    url: "https://nextjs.org/",
+    author: "Vercel",
+    num_comments: 7,
+    points: 5,
+    objectId: 5,
+  },
 ];
 
 function App() {
