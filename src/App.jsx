9087fa3e6dc561b2a21e58545816603cf3ea54df
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 11:02:25 +0530

    Creating object

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-const title = 'Bhoomi'
+const welcome = {
+  greeting: "Hey",
+  title: "Bhoomi",
+};
 
 function App() {
   return (
-    <div style={{"textAlign": "center"}}>
-      <h1>Hello {title}</h1>
-
+    <div style={{ textAlign: "center" }}>
+      <h1>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor="search">Search : </label>
-      &ensp; <input id='search' type="text" />
+      &ensp; <input id="search" type="text" />
     </div>
   );
 }
