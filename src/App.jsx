a0fb652c64751767278ac0ac2fbb3f33aba07318
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 11:13:55 +0530

    mapping object array into string array

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,35 @@ import React from "react";
 // }
 const list = [
   {
-    title: 'React',
-    url: 'https://react.js.org/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://react.js.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectId: 0
+    objectId: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
-    objectId: 1
+    objectId: 1,
   },
-]
+];
 
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Hello {getTitle('Bhoomi')}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search : </label>
       &ensp; <input id="search" type="text" />
+      <hr />
+      {list.map((item) => {
+        return (
+          <li style={{ fontSize: "2rem", fontWeight: "bold" }}>{item.title}</li>
+        );
+      })}
     </div>
   );
 }
