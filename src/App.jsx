14f2de56a843c8f38a79d7b4d366320b68218580
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 11:09:03 +0530

    Declaring list object

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ import React from "react";
 //   title: "Bhoomi",
 // };
 
-function getTitle(title){
-  return title;
-}
+// function getTitle(title){
+//   return title;
+// }
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  },
+]
 
 function App() {
   return (
