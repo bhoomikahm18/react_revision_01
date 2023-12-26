f4812b19878adc4056e26625c28b5da7e93b1989
Bhoomika <bhoomikahm18@gmail.com>
2023-12-26 12:52:05 +0530

    Assigning handler inline

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import './list.css';
+import "./list.css";
 
 export default function list(props) {
   return (
     <div>
       {props.list.map((item) => (
         <li className="list" key={item.objectId}>
-          <a href={item.url}>{item.title}</a> | {item.author} | Points: {item.points}
+          <a href={item.url}>{item.title}</a> | {item.author} | Points:{" "}
+          {item.points}
+          <button
+            style={{ fontSize: "1.2rem" }}
+            onClick={() => {
+              props.onRemoveItem(item);
+            }}
+          >
+            Dismiss
+          </button>
         </li>
       ))}
     </div>
   );
 }
-
